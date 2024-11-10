5f64de12c45eeaa95c61469f061a8b0d27808cd2
Peter Walsh <peterwalsh@Peters-MacBook-Pro.local>
2024-11-10 09:16:11 -0500

    Test'

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,7 @@ import { add, sub } from ".";
 describe("add", () => {
 
   it("should add the two numbers together", () => {
-    // expect(add(1, 2)).toBe(3)
+    expect(add(1, 2)).toBe(3)
   })
 
 })
@@ -11,7 +11,7 @@ describe("add", () => {
 describe("sub", () => {
 
   it("should subtract the first number from the second number", () => {
-    expect(sub(1, 2)).toBe(1);
+    // expect(sub(1, 2)).toBe(1);
   })
   
 })
